f0557cb2287475abda5dcc6f4f6428bc3ff31468
Akash Deokar <akashdeokar88@gmail.com>
2023-07-15 19:17:13 +0530

    Implement Navbar , Banner , Movies

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
 import Watchlist from './Components/Watchlist';
+import { BrowserRouter , Routes , Route } from 'react-router-dom' 
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Banner />
-      <Movies />
-      <Watchlist />
+      <BrowserRouter>
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Banner />
+              <Movies />
+            </>
+          }>
+          </Route>
+
+          <Route path='/watchlist' element={
+            <Watchlist />
+          }>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Movies = () => {
   return (
-    <div>Movies</div>
+    <div className='p-5'>
+      <div className='text-center font-bold text-2xl m-5'>
+        Trending Movies
+      </div>
+      <div className='flex flex-wrap justify-around gap-8'>
+        <div className='h-[40vh] w-[200px] rounded-xl bg-center bg-cover hover:scale-110 transition-transform duration-300 hover:cursor-pointer'
+          style={{
+            backgroundImage :`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`
+          }}>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
